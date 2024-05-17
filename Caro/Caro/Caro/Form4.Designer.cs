accb6cc0d922f84146395d5326c478145c2cd5d9
Linh Nguyen <97336307+nynhh01@users.noreply.github.com>
2024-05-17 23:01:08 +0700

    Add files via upload

---
+++ Caro/Caro/Caro/Form4.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Caro
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.txtTimePlayer2 = new System.Windows.Forms.TextBox();
+            this.txtTimePlayer1 = new System.Windows.Forms.TextBox();
+            this.txtMess = new System.Windows.Forms.TextBox();
+            this.txtNamePlayer2 = new System.Windows.Forms.TextBox();
+            this.txtNamePlayer1 = new System.Windows.Forms.TextBox();
+            this.đToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tiếpTụcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(-7, 176);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(947, 394);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.txtTimePlayer2);
+            this.panel1.Controls.Add(this.txtTimePlayer1);
+            this.panel1.Controls.Add(this.txtMess);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.txtNamePlayer2);
+            this.panel1.Controls.Add(this.txtNamePlayer1);
+            this.panel1.Location = new System.Drawing.Point(-7, 14);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(947, 156);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.menuStrip1);
+            this.panel7.Location = new System.Drawing.Point(902, 8);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(37, 31);
+            this.panel7.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.đToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(37, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // txtTimePlayer2
+            // 
+            this.txtTimePlayer2.Location = new System.Drawing.Point(704, 110);
+            this.txtTimePlayer2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTimePlayer2.Name = "txtTimePlayer2";
+            this.txtTimePlayer2.Size = new System.Drawing.Size(100, 22);
+            this.txtTimePlayer2.TabIndex = 8;
+            this.txtTimePlayer2.Text = "Time";
+            this.txtTimePlayer2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTimePlayer1
+            // 
+            this.txtTimePlayer1.Location = new System.Drawing.Point(144, 110);
+            this.txtTimePlayer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTimePlayer1.Name = "txtTimePlayer1";
+            this.txtTimePlayer1.Size = new System.Drawing.Size(100, 22);
+            this.txtTimePlayer1.TabIndex = 7;
+            this.txtTimePlayer1.Text = "time";
+            // 
+            // txtMess
+            // 
+            this.txtMess.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMess.ForeColor = System.Drawing.Color.Red;
+            this.txtMess.Location = new System.Drawing.Point(328, 110);
+            this.txtMess.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMess.Name = "txtMess";
+            this.txtMess.Size = new System.Drawing.Size(279, 30);
+            this.txtMess.TabIndex = 6;
+            this.txtMess.Text = "Good !";
+            this.txtMess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNamePlayer2
+            // 
+            this.txtNamePlayer2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.txtNamePlayer2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNamePlayer2.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtNamePlayer2.Location = new System.Drawing.Point(541, 58);
+            this.txtNamePlayer2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNamePlayer2.Name = "txtNamePlayer2";
+            this.txtNamePlayer2.Size = new System.Drawing.Size(263, 34);
+            this.txtNamePlayer2.TabIndex = 1;
+            // 
+            // txtNamePlayer1
+            // 
+            this.txtNamePlayer1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.txtNamePlayer1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNamePlayer1.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtNamePlayer1.Location = new System.Drawing.Point(144, 57);
+            this.txtNamePlayer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNamePlayer1.Name = "txtNamePlayer1";
+            this.txtNamePlayer1.Size = new System.Drawing.Size(264, 34);
+            this.txtNamePlayer1.TabIndex = 0;
+            // 
+            // đToolStripMenuItem
+            // 
+            this.đToolStripMenuItem.BackgroundImage = global::Caro.Properties.Resources.menu;
+            this.đToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.đToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiếpTụcToolStripMenuItem,
+            this.thoátToolStripMenuItem});
+            this.đToolStripMenuItem.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.đToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.đToolStripMenuItem.Name = "đToolStripMenuItem";
+            this.đToolStripMenuItem.Padding = new System.Windows.Forms.Padding(10);
+            this.đToolStripMenuItem.Size = new System.Drawing.Size(24, 26);
+            // 
+            // tiếpTụcToolStripMenuItem
+            // 
+            this.tiếpTụcToolStripMenuItem.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.tiếpTụcToolStripMenuItem.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tiếpTụcToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.tiếpTụcToolStripMenuItem.Name = "tiếpTụcToolStripMenuItem";
+            this.tiếpTụcToolStripMenuItem.Size = new System.Drawing.Size(171, 28);
+            this.tiếpTụcToolStripMenuItem.Text = "Tiếp tục";
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.thoátToolStripMenuItem.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thoátToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(171, 28);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            // 
+            // panel6
+            // 
+            this.panel6.BackgroundImage = global::Caro.Properties.Resources.avt3;
+            this.panel6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel6.Location = new System.Drawing.Point(57, 43);
+            this.panel6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(60, 63);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            this.panel5.BackgroundImage = global::Caro.Properties.Resources.avt4;
+            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel5.Location = new System.Drawing.Point(811, 43);
+            this.panel5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(67, 63);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.BackgroundImage = global::Caro.Properties.Resources.o;
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Location = new System.Drawing.Point(484, 58);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(35, 34);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImage = global::Caro.Properties.Resources.x;
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Location = new System.Drawing.Point(424, 58);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(32, 33);
+            this.panel3.TabIndex = 2;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 581);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem đToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tiếpTụcToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtTimePlayer2;
+        private System.Windows.Forms.TextBox txtTimePlayer1;
+        private System.Windows.Forms.TextBox txtMess;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtNamePlayer2;
+        private System.Windows.Forms.TextBox txtNamePlayer1;
+    }
+}
